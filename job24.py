ebf0be36d912315a30a708a6d3847c6fb280b3af
nicolas-panis <nicolas.panis@laplateforme.io>
2023-03-01 10:57:49 +0100

    pts sur le job24

--- job24.py
+++ job24.py
@@ -1,4 +1,6 @@
-cpt = 0
+pts = 0
+myList = []
+myListPts = []
 with open('dico_france.txt','r') as file:
    
     # reading each line    
@@ -7,6 +9,7 @@ with open('dico_france.txt','r') as file:
    
         # reading each word        
         for word in line.split():
+            pts = 0
             cpt = 0
             # displaying the words           
             # print(word) 
@@ -17,8 +20,31 @@ with open('dico_france.txt','r') as file:
                     for j in range(len(str)):
                         if str[j] == word[i]:
                             cpt = cpt + 1
+                            if str[j] == "a" or str[j] == "e" or str[j] == "i" or str[j] == "n" or str[j] == "o" or str[j] == "r" or str[j] == "s" or str[j] == "t" or str[j] == "u" or str[j] == "l":
+                                pts = pts + 1
+                            elif str[j] == "d" or str[j] == "g" or str[j] == "m":
+                                pts = pts + 2
+                            elif str[j] == "b" or str[j] == "c" or str[j] == "p":
+                                pts = pts + 3
+                            elif str[j] == "f" or str[j] == "h" or str[j] == "v":
+                                pts = pts + 4   
+                            elif str[j] == "j" or str[j] == "q":
+                                pts = pts + 8
+                            elif str[j] == "k" or str[j] == "w" or str[j] == "x" or str[j] == "y" or str[j] == "z":
+                                pts = pts + 10
                             str = str.replace(str[j],"0")
                             
                 
                 if cpt == len(str):
+                    if myListPts == []:
+                        myListPts.append(pts)
+                        myList.append(word)
+                    elif min(myListPts) < pts:
+                        myListPts.append(pts)
+                        myList.append(word)
+                    else:
+                        myListPts.insert(0, pts)
+                        myList.insert(0,word)
                     print(word)
+    print(myList)
+    print(myListPts)
